f69e3073bb286063c5d3a0dc5777c38bccb4b4d9
Diurno <Diurno@C7PC10>
2015-04-29 09:02:04 +0200

    Primer commit
    Este es el primer commit de nuestro proyecto de prueba.

---
+++ src/paquete_Ines_Castel/Clase_Ines_Castel.java
@@ -0,0 +1,16 @@
+/*
+ * Esto es una prueba
+ */
+
+package paquete_Ines_Castel;
+
+/**
+ *
+ * @author Inés 
+ */
+public class Clase_Ines_Castel {
+    public static void main(String [] args ){
+        System.out.println("Hola mundo");
+    }
+      
+}
