2a3e10a00b7b6bbffcfd101313945e700f2ac01d
Diurno <Diurno@C7PC10>
2015-04-29 09:06:46 +0200

    Segundo commit
    Este es el segundo commit de nuestro proyecto de prueba.

--- src/paquete_Ines_Castel/Clase_Ines_Castel.java
+++ src/paquete_Ines_Castel/Clase_Ines_Castel.java
@@ -11,6 +11,10 @@ package paquete_Ines_Castel;
 public class Clase_Ines_Castel {
     public static void main(String [] args ){
         System.out.println("Hola mundo");
+        for (int i = 0; i < 10; i++){
+            System.out.println(i + " veces algo");
+        }
     }
+ }
       
-}
+
