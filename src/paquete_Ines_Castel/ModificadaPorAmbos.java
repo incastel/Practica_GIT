ba7415ef8685e65b6be0321f5e7333110c8e7a0d
Diurno <Diurno@C7PC10>
2015-05-06 09:02:46 +0200

    Segundo commit
    Este es el segundo commit de nuestro proyecto de prueba.

---
+++ src/paquete_Ines_Castel/ModificadaPorAmbos.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package paquete_Ines_Castel;
+
+/**
+ *
+ * @author Inés Castel
+ */
+public class ModificadaPorAmbos {
+    
+    /**
+     * @author Inés Castel
+     */
+    public void printHoli()
+    {
+        System.out.println("Holiiiiiii");
+    }
+    
+    
+}
