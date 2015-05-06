610614506d565da5a06af1d8cf2917d5ceaaa8ca
Diurno <Diurno@C7PC09>
2015-05-06 08:45:44 +0200

    commit pablo moreno
    clases añadidas al proyecto

---
+++ src/paquete_Pablo_Moreno/ClasePabloMoreno.java
@@ -0,0 +1,12 @@
+package paquete_Pablo_Moreno;
+
+/**
+ * 
+ * @author Diurno
+ */
+public class ClasePabloMoreno {
+    public static void main(String[] args)
+    {
+        System.out.println("pablo moreno");
+    }
+}
